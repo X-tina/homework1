98ab0b626273df40f2e7016d277a86c6e6748740
Kristine Yefimtseva <ke@anahoret.com>
2014-05-26 17:44:51 +0300

    start anagram

--- main/anagram.rb
+++ main/anagram.rb
@@ -1,7 +1,17 @@
 module Anagram
   extend self
 
-  def anagrams(str)
-    []
+  def anagrams
+    str = ["rats", "What", "star", "thaw", "Tars"]
+    #str1 = str.split
+  
+    str_word = str.map { |word| word.split("") }
+    puts str_word
+    str_letter = str_word.select { |letter| ("a".."z").include?(letter)}
+    puts str_letter
+
+    #p(str_letter == str_letter.sort)
   end
 end
+
+
