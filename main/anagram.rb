3a39e604c6609c6a0c310d754e5245437917a3a2
Kristine <kristinka89@gmail.com>
2014-05-27 13:01:55 +0300

    finish anagram

--- main/anagram.rb
+++ main/anagram.rb
@@ -1,16 +1,8 @@
 module Anagram
   extend self
 
-  def anagrams
-    str = ["rats", "What", "star", "thaw", "Tars"]
-    #str1 = str.split
-  
-    str_word = str.map { |word| word.split("") }
-    puts str_word
-    str_letter = str_word.select { |letter| ("a".."z").include?(letter)}
-    puts str_letter
-
-    #p(str_letter == str_letter.sort)
+  def anagrams(str)    
+    str.split.group_by { |word| word.downcase.split("").sort }.values
   end
 end
 
