ae60f3116e871787e262de8e481f02f75000c3c3
Kristine Yefimtseva <ke@anahoret.com>
2014-05-30 14:27:40 +0300

    Metaprogramming_start

--- main/metaprogramming.rb
+++ main/metaprogramming.rb
@@ -1,1 +1,33 @@
-metaprogramming.rb
+class Class
+
+  def attr_accessor_with_history(name1)
+    name = name1.to_s
+
+    define_method "#{name}" do
+      instance_variable_get("@#{name}")
+    end
+
+    define_method "#{name}_history" do 
+      instance_variable_get("@#{name}_history") || [nil]
+    end
+
+    define_method "#{name}=" do |value|
+      res = instance_variable_get("@#{name}_history")
+      res ||= [nil]
+      res << value
+      instance_variable_set("@#{name}_history", instance_variable_get("@#{name}"))
+      instance_variable_set("@#{name}", value)
+    end
+  end
+
+end
+
+class Foo
+  attr_accessor_with_history :bar
+end
+
+p = Foo.new
+p.bar = 2
+p.bar = 5
+p.bar = 8
+puts p.bar_history
